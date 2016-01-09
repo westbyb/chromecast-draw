e8546b98a4ff7ce3def537afb3b23ef98225c85f
Brian Westby <brian.westby@gmail.com>
2016-01-08 23:41:48 -0800

    game creation now works, can add players to game

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var game = require('./libs/game.js');
+var active_rooms = [];
 
 app.get('/', function(req,res){
-	res.sendFile(__dirname + '/index.html');
-	// res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	socket.on('new image', function(msg){
-		io.emit('new image', msg);
-	});
+  socket.on('new player', function(player_info){
+      console.log('New player: ' + player_info.name);
+      // console.log(active_rooms);
+      var current_game = get_room(player_info.room_code);
+      current_game.add_player(player_info.name);
+      io.emit('new player', player_info.name);
+  });
+
+  socket.on('new game', function(){
+    function generate_room_code(){
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+      for( var i=0; i < 4; i++ )
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      return text;
+    }
+
+    var room_code = generate_room_code();
+    //if the room code is already in use, generate a new one until you find one that isn't in use
+    while(active_rooms.indexOf(room_code) === 0) {
+      room_code = generate_room_code();
+    }
+
+    var g = new game(room_code);
+    active_rooms.push(g);
+    io.emit('new game', room_code);
+    console.log('new game. room code: ' + g.room_code);
+    // console.log('all games: ' + active_rooms);    
+  });
 });
 
 http.listen(3000, function(){
-	console.log('Server started listening on *:3000');
+    console.log('Server started listening on *:3000');
 });
+
+function get_room(room_code){
+  var desired_game = active_rooms.filter(function(ga){
+    return  ga.room_code === room_code;
+  });
+  return desired_game[0];
+}
---
+++ libs/game.js
@@ -0,0 +1,74 @@
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+// var socket = io();
+
+// function player(name){
+//   this.name = name;
+//   this.score = 0;
+
+//   /**
+//    * assigns photo to player
+//    */
+//   this.add_picture = function(picture){
+//     this.picture = picture;
+//   };
+
+//   *
+//    * @delta  {number} score difference from the round (+/-)
+   
+//   this.change_score = function(delta){
+//     this.score += delta;
+//   };
+
+//   this.submit_drawing = function(drawing){
+//     //todo: figure this out
+//   };
+// }
+// module.exports.player = player;
+
+module.exports = function game(room_code){
+    this.room_code = room_code;
+    this.players = [];
+    this.available_colors = []; //todo: populate with colors
+    this.scoreboard = [];
+
+    /**
+     * returns -1 on failure, 0 on success
+     * @param {[type]}
+     * @param {[type]}
+     */
+    this.add_player = function(name){
+      //don't add player if player with same name already exists
+      if (this.get_player(name).length > 0){
+        console.log('Player with name ' + name + ' already exists in room ' + this.room_code);
+        return -1;
+      }
+
+      // var new_player = new player(name);
+      // this.players.push(new_player);
+      console.log('Added player ' + name + ' to game ' + this.room_code);
+      return 0;
+    };
+
+    this.get_player = function(name){
+      var player = this.players.filter(function(p){
+        return  p.name === name;
+      });
+      return player;
+    };
+
+    /**
+     * updates the leaderboard to reflect current standing
+     */
+    this.update_scoreboard = function(){
+      this.scoreboard.sort(function(p1, p2){
+        if(p1.score > p2.score) { return 1; }
+        if(p1.score < p2.score) { return -1; }
+        return 0;
+      });
+    };
+
+    this.test = function(){
+      console.log('?!');
+    };
+};
